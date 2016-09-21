8ef1395ad0f40d6642faeb86161e09036a24b865
Jesse Himmelstein <jesse@heavenstone.net>
2016-09-21 17:34:04 +0200

    WIP: filling in email

--- src/clj/same_day_different_lives/notification.clj
+++ src/clj/same_day_different_lives/notification.clj
@@ -1,6 +1,7 @@
 (ns same-day-different-lives.notification
   (:require [org.httpkit.server :as httpkit]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [same-day-different-lives.config :refer [config]]))
 
 
 ;; -------------------------
@@ -40,9 +41,34 @@
                                   (println "channel closed: " status " for user " user-id)
                                   (swap! users-to-channels dissoc user-id))))))
 
+
+(defn prepare-email [notification]
+  (condp = (:type notification)
+    :new-response {:text "The other player has answered the question."
+                   :link (str "/match/" (:match-id notification))}
+    :unlocked-challenge {:text "There's a new question to answer."
+                         :link (str "/match/" (:match-id notification))} 
+    :ended-match {:text "Your journal has ended."
+                  :link (str "/match/" (:match-id notification))}
+    :created-match {:text "You have been paired up to make a new journal."
+                    :link (str "/match/" (:match-id notification))}
+    (throw  (Error. (str "ERROR unknown notification type" (:type notification))))))
+
+(defn make-email-body [email-fields]
+  (str "Hello Same Day Different Lives player, \n\n"
+       (:text email-fields) "\n\n"
+       "Go to " (get-in config [:email :link-prefix] " to find out more.\n\n")
+       "Yours truly,\n"
+       "The SDDL Robot"))
+
 (defn send-by-email! [user-id notification]
   ; TODO: actually send email :)
-  (prn "Sending mail to " user-id ": " notification))
+  (prn "Sending mail to " user-id ": " notification)
+  (let [email-fields (prepare-email notification)
+        email-body (make-email-body email-fields)]
+    ; TODO: actually send email
+    ))
+
 
 (defn send! [user-id notification]
   (if (user-is-connected? user-id)
